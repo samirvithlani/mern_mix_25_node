0d049d4032f7e3e74929d7a44b77f6f6c488d50b
samirvithlani <samir.vithlani83955@gmail.com>
2025-10-27 14:42:28 +0530

    reids

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express") //express module..
 const mongoose  = require("mongoose")
 const app = express()
+const Redis = require("ioredis")
+const {Queue} = require("bullmq")
 
 //global middlware..
 app.use(express.json()) //req.body json type excpet..
@@ -22,6 +24,25 @@ app.use("/upload",uploadRoutes)
 mongoose.connect("mongodb://127.0.0.1:27017/mern_club_mix").then(()=>{
     console.log("database connected successfully!!")
 })
+
+
+
+//redis connection
+const redisConnection = new Redis({
+    host:"127.0.0.1",
+    port:6379
+})
+//queue..
+const myQueue = new Queue("taskQueue",{connection:redisConnection})
+
+app.post("/set-batch",async(req,res)=>{
+    const {name} = req.body
+    await myQueue.add("task",{name},{delay:0})
+    res.json({
+        message:"batch set successfully..."
+    })
+})
+
 
 //server creation...
 const PORT = 3000
---
+++ worker.js
@@ -0,0 +1,26 @@
+const {Worker} = require("bullmq")
+const Redis  = require("ioredis")
+const { resolve } = require("path")
+
+const redisConnection = new Redis({
+    host:"127.0.0.1",
+    port:6379,
+    maxRetriesPerRequest:null
+})
+
+const worker = new Worker(
+    "taskQueue",
+    async(job)=>{
+        //job...        
+        console.log(`procesing job for ${job.data.name}`)
+        await new Promise((resolve)=>setTimeout((resolve),5000))
+        console.log(`job done for ${job.data.name}`)
+    },
+    {connection:redisConnection}
+)
+worker.on("completed",(job)=>{
+    console.log("job successfully done ",job.id)
+})
+worker.on("failed",(job,err)=>{
+    console.log("job failed...",job.id)
+})
